0b4ffbfe1a817c420f83a2a6dfee253d6d2bfb64
Alon <ageva10@gmail.com>
2021-02-12 22:16:44 +0200

    weather-test

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,58 +4,6 @@ import path from 'path'
 import moment from 'moment'
 import { getIndexes, getNewId, getWeatherData, isValidCityAndDate } from '../helper'
 
-//////////////////////////////////////////
-/*
-const date = moment().utcOffset(0)
-date.set({ hour: 0, minute: 0, second: 0, millisecond: 0 })
-console.log(date.toISOString())
-console.log(date.format())
-
-// 7 Days ago
-const weather = path.join(__dirname, '../data/weather.json')
-
-const file = path.join(__dirname, '../data/city.list.json')
-const content = JSON.parse(fs.readFileSync(file, 'utf8'))
-const array = []
-
-for (let i = 0; i < Math.ceil(content.length / 7); i++) {
-  const isShow = Boolean(Math.floor(Math.random() * 2))
-  for (let y = 1; y <= 7; y++) {
-
-    const data = {
-      id: content[i].id,
-      city_name: content[i].name,
-      date: moment(date, 'DD-MM-YYYY').add(-y, 'd'),
-      show: isShow,
-      weather: {
-        temp: Number((Math.random() * (30 - 20 + 1) + 20).toFixed(2)),
-        temp_min: Number((Math.random() * (20 - 10 + 1) + 10).toFixed(2)),
-        temp_max: Number((Math.random() * (50 - 30 + 1) + 30).toFixed(2)),
-        feels_like: Number((Math.random() * (50 - 20 + 1) + 20).toFixed(2)),
-        pressure: Number((Math.random() * (1010 - 1000 + 1) + 1000).toFixed(2)),
-        humidity: Number((Math.random() * (80 - 70 + 1) + 70).toFixed(2)),
-        wind: {
-          speed: Number((Math.random() * (15 - 1 + 1) + 1).toFixed(1)),
-          deg: Number((Math.random() * (250 - 230 + 1) + 230).toFixed(2)),
-        },
-        clouds: {
-          all: Number((Math.random() * (100 - 90 + 1) + 90).toFixed(1))
-        }
-      }
-    }
-
-    array.push(data)
-
-  }
-}
-
-fs.writeFileSync(weather, JSON.stringify(array))
-
-console.log(array)
-
- */
-//////////////////////////////////////////
-
 let weatherData = null
 const weatherFile = path.join(__dirname, '../data/weather.json')
 
